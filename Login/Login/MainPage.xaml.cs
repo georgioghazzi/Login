dd7ca198a94d7169a6865ee17529dba001d7322f
Georgio Ghazzi <ghazzi94@gmail.com>
2018-04-28 00:44:15 +0300

    Add project files.

---
+++ Login/Login/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Login
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private void LoginButton_Clicked(Object sender,EventArgs e)
+        {
+            bool isEmailEmpty = String.IsNullOrEmpty(email.Text);
+            bool isPasswordEmpty = String.IsNullOrEmpty(password.Text);
+            String emailAdd = email.ToString();
+            String pass = password.ToString();
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+               
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            
+
+        }
+
+    }
+}
